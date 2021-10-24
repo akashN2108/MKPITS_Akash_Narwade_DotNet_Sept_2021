aed289eb28eafa7960ea1ab405e1df29dbc08181
akash narwade <narwadeakash.2108@gmail.com>
2021-10-24 15:01:36 +0530

    Upadated Ass12oct

---
+++ C_Programs/Assignment_12Oct/H12_oct_input_arryan_reverse.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<malloc.h>
+ int main()
+ {
+   int *n;
+   
+   printf("How many number you want to print:");
+   {
+     scanf("%d",n);
+   }
+   return 0;
+  }
+  
+  int i,n;
+  for(i=n;i>=1;i--)
+  printf("the number =%d");
+  {
+    printf("%d",i);
+  }
+   
+   
+  
+
