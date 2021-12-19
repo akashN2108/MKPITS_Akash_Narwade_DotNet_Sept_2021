401beac4f8d375881527fb4ece35ee1aaa05f37a
akash narwade <narwadeakash.2108@gmail.com>
2021-12-19 17:26:14 +0530

    Conversion

---
+++ C_sharpe/C_Dec19_celcius_to_farenheti_scale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_Dec19_celcius_to_farenheti_scale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celcius = 100, f;
+            Console.WriteLine("enter the temperature in celcius");
+            celcius = Convert.ToInt32(Console.ReadLine());
+            f = (celcius * 1.8) + 32;
+            Console.WriteLine("Temperature into Farenheti scale={0:f2}\n", f);
+
+        }
+    }
+}
