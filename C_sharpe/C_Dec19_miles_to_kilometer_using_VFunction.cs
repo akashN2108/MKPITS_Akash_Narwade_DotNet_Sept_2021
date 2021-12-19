f6c7940f3e2c167e6010cdbaf9ee724c7b3aef8f
akash narwade <narwadeakash.2108@gmail.com>
2021-12-19 16:51:03 +0530

    conversion

---
+++ C_sharpe/C_Dec19_miles_to_kilometer_using_VFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace C_Dec19_miles_to_kilometer_using_VFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int miles;
+            Console.WriteLine("\nEnter the miles ");
+            miles = Convert.ToInt32(Console.ReadLine());
+            miles_km(miles);
+        }
+        public static void miles_km(int miles)
+        {
+            double km = 1.61 * miles;
+
+
+            Console.WriteLine("{0} miles = {1:f2} km", miles, km);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_sharpe/C_Dec19_rupees_to_dollar_conversion_using_function.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C_Dec19_rupees_to_dollar_conversion_using_function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Rs;
+            Console.WriteLine("\nEnter the Rupees ");
+            Rs = Convert.ToInt32(Console.ReadLine());
+            Rs_Dollar(Rs);
+            Rs_Dollar(79);
+        }
+            public static void Rs_Dollar(int Rs)
+        {
+            double Dollar = 0.013 * Rs;
+
+
+            Console.WriteLine("{0}rupees = {1:f3}dollar", Rs, Dollar);
+
+        }
+
+    }
+}
