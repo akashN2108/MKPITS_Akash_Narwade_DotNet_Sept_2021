9229f58162022a6aa02f68dcb1a951ca6df80fe6
akash narwade <narwadeakash.2108@gmail.com>
2022-01-06 14:12:22 +0530

    Get Post Method

---
+++ DotNet_Programs/WebForm2.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Get_PostExample1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        string s3, s4;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            s3 = Request.Form.Get("Name").ToString();
+            s4 = Request.Form.Get("City").ToString();
+
+            Label2.Text = s3;
+            Label3.Text = s4;
+        }
+    }
+}
